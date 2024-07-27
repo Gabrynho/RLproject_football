dec8678706d3a8a8cf1df9e5acf3023a8f55d414
Gabrynho <masellagabriel@gmail.com>
2024-07-27 16:50:52 +0200

    Training CR7 model, optimizing training algorithm, added score per step plot

---
+++ DQN/score_per_step_plot.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# List of file names
+file_names = ['DQN/level1_score_per_step.csv', 'DQN/level2_score_per_step.csv', 'DQN/level3_score_per_step.csv', 'DQN/level4_score_per_step.csv']
+
+# Initialize an empty list to store the data
+data = []
+
+# Read the data from each file and append it to the list
+for file_name in file_names:
+    df = pd.read_csv(file_name)
+    data.append(df['Score per Step'])
+
+fig, axs = plt.subplots(2, 2, figsize=(12, 8))
+axs = axs.flatten()
+
+for i, d in enumerate(data):
+    axs[i].plot(d, label=f'Level {i+1}')
+    axs[i].set_xlabel('Episode')
+    axs[i].set_ylabel('Score per Step')
+    axs[i].set_title(f'Score per Step vs Episode (Level {i+1})')
+    axs[i].legend()
+
+    x = np.arange(len(d))
+    y = d
+
+    best_degree = None
+    min_chi2 = float('inf')
+
+    # Test del chi-quadro per selezionare il miglior grado del polinomio
+    for degree in range(2, 15):
+        coeffs = np.polyfit(x, y, degree)
+        poly_eq = np.poly1d(coeffs)
+        fitted_values = poly_eq(x)
+        chi2_value = np.sum(((y - fitted_values) ** 2) / fitted_values)
+
+        if chi2_value < min_chi2:
+            min_chi2 = chi2_value
+            best_coeffs = coeffs
+
+    poly_eq = np.poly1d(best_coeffs)
+    axs[i].plot(x, poly_eq(x), color='red', linestyle='--')
+    degree = len(best_coeffs) - 1
+    coeffs_str = f'Grade {degree}'
+    axs[i].legend(['Agent during Training', f'Fit: Degree {degree}, $\\chi^2$ = {min_chi2:.2f}'])
+
+plt.tight_layout()
+plt.show()
--- DQN/trainAgent.py
+++ DQN/trainAgent.py
@@ -6,10 +6,7 @@ import gfootball.env as football_env
 
 ############################################
 # Utility functions
-############################################
-
-def add_observation(observation):
-    observations.append(observation)
+############################################   
 
 def save_observations_to_csv(folder, filename):
     if not os.path.exists(folder):
@@ -20,13 +17,17 @@ def save_observations_to_csv(folder, filename):
     df = pd.DataFrame(observations)
     df.to_csv(file_path, index=False)
 
+def convert_scores_to_csv(scst, level):
+    df1 = pd.DataFrame(scst, columns=['Score per Step'])
+    df1.to_csv(f'DQN/level{level}_score_per_step.csv', index=False)
+
 ############################################
 # Inizialization
 ############################################
 
 folder = 'DQN/episode_observations'
 
-# Create the environment
+# Create the environment of reference
 env = football_env.create_environment(env_name="gm_level1", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
 
 # Initialize the DQN agent
@@ -41,277 +42,136 @@ if os.path.exists('DQN/CR7_model.pth'):
 # Number of episodes
 num_episodes = 1000
 num_test = 100
+
+def train_agent(level, agent, num_episodes):
+
+    print("############################################")
+    print(f"Training on level {level}")
+    scst = []
+
+    env = football_env.create_environment(env_name=f"gm_level{level}", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
+
+    # Training loops
+    for i in range(num_episodes): # number of episodes
+        done = False
+        observation = env.reset()
+        score = 0
+        steps = 0
+        while not done:
+            action = agent.choose_action(observation)
+            next_observation, reward, done, info = env.step(action)
+            score += reward
+            agent.store_transition(observation, action, reward, next_observation, done)
+            agent.learn()
+            observation = next_observation
+            steps += 1
+            if score > 1 and done==True:
+                print("Goal!")
+        scst.append(score/steps)
+        print(f'Episode {i}, Score: {score}, Steps: {steps}')
+        if i % 10 == 0:
+            agent.save_model('DQN/CR7_model.pth')
+    env.close()
+    agent.save_model('DQN/CR7_model.pth')
+
+    print(f"Training on level {level} completed")
+    
+    return scst
+
+def test_agent(level, agent, num_test):
+
+    print("############################################")
+    print(f"Testing on level {level}")
+    observations = []
+
+    env = football_env.create_environment(env_name=f"gm_level{level}", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
+    
+    # Test loop
+    for episode in range(num_test):
+        # Reset the environment and collect observations
+        observation = env.reset()
+        observations.append(observation)
+        done = False
+        score = 0
+        steps = 0
+        while not done:
+            action = agent.choose_action(observation)
+            observation, reward, done, info = env.step(action)
+            observations.append(observation)
+            # Update the score
+            score += reward
+            steps += 1
+            if score > 1 and done == True:
+                print("Goal!")
+                save_observations_to_csv(folder, f'level1_episode_{episode+1}_observations.csv')
+        print(f"Game {episode+1}: Score = {score}, Steps = {steps}")
+        observations = []
+    
+    env.close()
+    print(f"Testing on level {level} completed")
 ############################################
 # Level 1 Training
 ############################################
 
-# Initialize the environment
-level1_env = football_env.create_environment(env_name="gm_level1", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Training on level 1")
-scst1 = []
+# Train
+scst1 = train_agent(1, CR7, num_episodes)
 
-# Training loops
-for i in range(num_episodes): # number of episodes
-    done = False
-    observation = level1_env.reset()
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        next_observation, reward, done, info = level1_env.step(action)
-        score += reward
-        CR7.store_transition(observation, action, reward, next_observation, done)
-        CR7.learn()
-        observation = next_observation
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-    scst1.append(score/steps)
-    print(f'Episode {i}, Score: {score}, Steps: {steps}')
-    if i % 10 == 0:
-        CR7.save_model('DQN/CR7_model.pth')
-level1_env.close()
-CR7.save_model('DQN/CR7_model.pth')
-df1 = pd.DataFrame(scst1, columns=['Score per Step'])
-df1.to_csv('DQN/level1_score_per_step.csv', index=False)
-print("Training on level 1 completed")
-print("############################################")
+convert_scores_to_csv(scst1, 1)
 
 ############################################
 
-# Test the agent and see how it acts
-test_env1 = football_env.create_environment(env_name="gm_level1", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Testing on level 1")
-observations = []
-
-# Test loop
-for episode in range(num_test):
-    # Reset dell'ambiente e raccolta delle osservazioni
-    observation = test_env1.reset()
-    add_observation(observation)
-    done = False
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        observation, reward, done, info = test_env1.step(action)
-        add_observation(observation)
-        # Update the score
-        score += reward
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-            save_observations_to_csv(folder, f'level1_episode_{episode+1}_observations.csv')
-    print(f"Game {episode+1}: Score = {score}, Steps = {steps}")
-    observations = []
-test_env1.close()
-print("Testing on level 1 completed")
-print("############################################")
+# Test 
+test_agent(1, CR7, num_test)
 
 ############################################
 # Level 2 Training
 ############################################
 
-# Initialize the environment
-level2_env = football_env.create_environment(env_name="gm_level2", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Training on level 2")
-scst2 = []
+# Train
+scst2 = train_agent(2, CR7, num_episodes)
 
-# Training loops
-for i in range(num_episodes): # number of episodes
-    done = False
-    observation = level2_env.reset()
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        next_observation, reward, done, info = level2_env.step(action)
-        score += reward
-        CR7.store_transition(observation, action, reward, next_observation, done)
-        CR7.learn()
-        observation = next_observation
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-    scst2.append(score/steps)
-    print(f'Episode {i}, Score: {score}, Steps: {steps}')
-    if i % 10 == 0:
-        CR7.save_model('DQN/CR7_model.pth')
-level2_env.close()
-CR7.save_model('DQN/CR7_model.pth')
-df2 = pd.DataFrame(scst2, columns=['Score per Step'])
-df2.to_csv('DQN/level2_score_per_step.csv', index=False)
-print("Training on level 2 completed")
-print("############################################")
+convert_scores_to_csv(scst2, 2)
 
 ############################################
 
-# Test the agent and see how it acts
-test_env2 = football_env.create_environment(env_name="gm_level2", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Testing on level 2")
-observations = []
-
-# Test loops
-for episode in range(num_test):
-    # Reset dell'ambiente e raccolta delle osservazioni
-    observation = test_env2.reset()
-    add_observation(observation)
-    done = False
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        observation, reward, done, info = test_env2.step(action)
-        add_observation(observation)
-        # Update the score
-        score += reward
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-            save_observations_to_csv(folder, f'level2_episode_{episode+1}_observations.csv')
-    print(f"Game {episode+1}: Score = {score}, Steps = {steps}")
-    observations = []
-test_env2.close()
-print("Testing on level 2 completed")
-print("############################################")
+# Test 
+test_agent(2, CR7, num_test)
 
 ############################################
 # Level 3 Training
 ############################################
 
-# Initialize the environment
-level3_env = football_env.create_environment(env_name="gm_level3", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Training on level 3")
-scst3 = []
+# Train
+scst3 = train_agent(3, CR7, num_episodes)
 
-# Training loops
-for i in range(num_episodes): # number of episodes
-    done = False
-    observation = level3_env.reset()
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        next_observation, reward, done, info = level3_env.step(action)
-        score += reward
-        CR7.store_transition(observation, action, reward, next_observation, done)
-        CR7.learn()
-        observation = next_observation
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-    scst3.append(score/steps)
-    print(f'Episode {i}, Score: {score}, Steps: {steps}')
-    if i % 10 == 0:
-        CR7.save_model('DQN/CR7_model.pth')
-level3_env.close()
-CR7.save_model('DQN/CR7_model.pth')
-df3 = pd.DataFrame(scst3, columns=['Score per Step'])
-df3.to_csv('DQN/level3_score_per_step.csv', index=False)
-print("Training on level 3 completed")
-print("############################################")
+convert_scores_to_csv(scst3, 3)
 
 ############################################
 
-# Test the agent and see how it acts
-test_env3 = football_env.create_environment(env_name="gm_level3", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Testing on level 3")
-observations = []
-
-# Test loops
-for episode in range(num_test):
-    # Reset dell'ambiente e raccolta delle osservazioni
-    observation = test_env3.reset()
-    add_observation(observation)
-    done = False
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        observation, reward, done, info = test_env3.step(action)
-        add_observation(observation)
-        # Update the score
-        score += reward
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-            save_observations_to_csv(folder, f'level3_episode_{episode+1}_observations.csv')
-    print(f"Game {episode+1}: Score = {score}, Steps = {steps}")
-    observations = []
-test_env3.close()
-print("Testing on level 3 completed")
-print("############################################")
+# Test 
+test_agent(3, CR7, num_test)
 
 ############################################
 # Level 4 Training
 ############################################
 
-# Initialize the environment
-level4_env = football_env.create_environment(env_name="gm_level4", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Training on level 4")
-scst4 = []
+# Train
+scst4 = train_agent(4, CR7, num_episodes)
 
-# Training loops
-for i in range(num_episodes): # number of episodes
-    done = False
-    observation = level4_env.reset()
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        next_observation, reward, done, info = level4_env.step(action)
-        score += reward
-        CR7.store_transition(observation, action, reward, next_observation, done)
-        CR7.learn()
-        observation = next_observation
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-    scst4.append(score/steps)
-    print(f'Episode {i}, Score: {score}, Steps: {steps}')
-    if i % 10 == 0:
-        CR7.save_model('DQN/CR7_model.pth')
-level4_env.close()
-CR7.save_model('DQN/CR7_model.pth')
-df4 = pd.DataFrame(scst4, columns=['Score per Step'])
-df4.to_csv('DQN/level4_score_per_step.csv', index=False)
-print("Training on level 4 completed")
-print("############################################")
+convert_scores_to_csv(scst4) , 4
 
 ############################################
 
-# Test the agent and see how it acts
-test_env4 = football_env.create_environment(env_name="gm_level4", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
-print("Testing on level 4")
-observations = []
-
-# Test loops
-for episode in range(num_test):
-    # Reset dell'ambiente e raccolta delle osservazioni
-    observation = test_env4.reset()
-    add_observation(observation)
-    done = False
-    score = 0
-    steps = 0
-    while not done:
-        action = CR7.choose_action(observation)
-        observation, reward, done, info = test_env4.step(action)
-        add_observation(observation)
-        # Update the score
-        score += reward
-        steps += 1
-        if score > 1 & done==True:
-            print("Goal!")
-            save_observations_to_csv(folder, f'level4_episode_{episode+1}_observations.csv')
-    print(f"Game {episode+1}: Score = {score}, Steps = {steps}")
-    observations = []
-test_env4.close()
-print("Testing on level 4 completed")
-print("############################################")
+# Test 
+test_agent(4, CR7, num_test)
 
 ############################################
 # Level 5 Training
 ############################################
+
+# Number of episodes
+num_episodes = 100
+num_test = 10
 
 # Initialize the environment
 level5_env = football_env.create_environment(env_name="gm_level5", representation='simple115', stacked=False, render=False, rewards='scoring,checkpoints')
@@ -356,14 +216,14 @@ observations = []
 for episode in range(num_test):
     # Reset dell'ambiente e raccolta delle osservazioni
     observation = test_env5.reset()
-    add_observation(observation)
+    observations.append(observation)
     done = False
     score = 0
     steps = 0
     while not done:
         action = CR7.choose_action(observation)
         observation, reward, done, info = test_env5.step(action)
-        add_observation(observation)
+        observations.append(observation)
         # Update the score
         score += reward
         steps += 1
@@ -372,6 +232,6 @@ for episode in range(num_test):
             save_observations_to_csv(folder, f'level5_episode_{episode+1}_observations.csv')
     print(f"Game {episode+1}: Score = {score}, Steps = {steps}")
     observations = []
-test_env4.close()
+test_env5.close()
 print("Testing on level 5 completed")
 print("############################################")
